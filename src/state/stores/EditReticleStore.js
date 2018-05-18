67248c587b9d19288330cf763c20e1dff2795023
Derek Knox <derek-knox@users.noreply.github.com>
2018-05-18 16:56:10 -0700

    refactored id removal

--- src/state/stores/EditReticleStore.js
+++ src/state/stores/EditReticleStore.js
@@ -7,14 +7,14 @@ import { getEditAreaInfo } from '../../utils/reticleUtils';
 export class EditReticleStore {
 
     @observable items = [
-        new EditControlModel({ id: 0, label: 'Radius', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Radius, min: 1, max: 1000 } }),
-        new EditControlModel({ id: 1, label: 'Thickness', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Thickness, min: 1, max: 150 } }),
-        new EditControlModel({ id: 2, label: 'Divisions', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Divisions, min: 0, max: 180 } }),
-        new EditControlModel({ id: 3, label: 'Spacing', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Spacing, min: 1, max: 359 } }),
-        new EditControlModel({ id: 4, label: 'Rotation', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Rotation, min: 1, max: 360 } }),
-        new EditControlModel({ id: 5, label: 'Graphic', type: EditControlModel.Type.List, settings: { reticleProp: ReticleModel.SettingType.Graphic, items: [] } }),
-        new EditControlModel({ id: 6, label: 'Direction', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Direction, min: 1, max: 360 } }),
-        new EditControlModel({ id: 7, label: 'Scale', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Scale, min: 1, max: 5 } })
+        new EditControlModel({ label: 'Radius', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Radius, min: 1, max: 1000 } }),
+        new EditControlModel({ label: 'Thickness', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Thickness, min: 1, max: 150 } }),
+        new EditControlModel({ label: 'Divisions', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Divisions, min: 0, max: 180 } }),
+        new EditControlModel({ label: 'Spacing', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Spacing, min: 1, max: 359 } }),
+        new EditControlModel({ label: 'Rotation', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Rotation, min: 1, max: 360 } }),
+        new EditControlModel({ label: 'Graphic', type: EditControlModel.Type.List, settings: { reticleProp: ReticleModel.SettingType.Graphic, items: [] } }),
+        new EditControlModel({ label: 'Direction', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Direction, min: 1, max: 360 } }),
+        new EditControlModel({ label: 'Scale', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Scale, min: 1, max: 5 } })
     ];
     @observable isDrawing = false;
     @observable editAreaInfo = null;
