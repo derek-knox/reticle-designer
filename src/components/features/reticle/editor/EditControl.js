4c933db4f174e60bb27a5934910e632d3a782030
Derek Knox <derek-knox@users.noreply.github.com>
2018-05-21 15:12:23 -0700

    updated strategy for updating controlInFocus via automatic settings.val change

--- src/components/features/reticle/editor/EditControl.js
+++ src/components/features/reticle/editor/EditControl.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { action } from 'mobx';
 import {inject, observer} from 'mobx-react';
 import keydown from 'react-keydown';
 
@@ -9,12 +10,17 @@ import EditControlWidget from './EditControlWidget';
 @keydown('up', 'right', 'down', 'left', 'shift+up', 'shift+right', 'shift+down', 'shift+left')
 export default class EditControl extends Component {
 
+    @action.bound onMouseEnterControl(e, payload) {
+        this.props.stores.reticlesStore.reticleInFocus.controlInFocus = payload;
+    }
+
     render() {
 
         const item = this.props.item;
 
         return (
-            <div className={'reticle-editor-control-row ' + (this.props.controlInFocus.id === item.id ? 'is-selected' : '')}>
+            <div className={'reticle-editor-control-row ' + (this.props.controlInFocus.id === item.id ? 'is-selected' : '')}
+                 onMouseEnter={(e) => this.onMouseEnterControl(e, item)}>
                 
                 <div className='reticle-editor-control-label'>
                     {item.label}
--- src/state/models/ReticleModel.js
+++ src/state/models/ReticleModel.js
@@ -1,4 +1,4 @@
-import { action, computed, observable } from 'mobx';
+import { action, computed, observe, observable } from 'mobx';
 import { uniqueId } from 'lodash';
 
 import { EditControlModel } from './EditControlModel';
@@ -36,26 +36,29 @@ export class ReticleModel {
     this.id = uniqueId();
     this.label = 'Layer ' + ++ReticleModel.layerId;
 
-    let isClone = typeof payload.radius === "object";
-    this.radius = new EditControlModel({ label: 'Radius', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Radius, val: isClone ? payload.radius.settings.val : payload.radius || 10, min: 1, max: 1000 } }),
-    this.thickness = new EditControlModel({ label: 'Thickness', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Thickness, val: isClone ? payload.thickness.settings.val : 4, min: 1, max: 150 } }),
-    this.divisions = new EditControlModel({ label: 'Divisions', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Divisions, val: isClone ? payload.divisions.settings.val : 0, min: 0, max: 180 } }),
-    this.spacing = new EditControlModel({ label: 'Spacing', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Spacing, val: isClone ? payload.spacing.settings.val : 10, min: 1, max: 359 } }),
-    this.rotation = new EditControlModel({ label: 'Rotation', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Rotation, val: isClone ? payload.rotation.settings.val : 1, min: 1, max: 360 } }),
-    this.graphic = new EditControlModel({ label: 'Graphic', type: EditControlModel.Type.List, settings: { reticleProp: ReticleModel.SettingType.Graphic, val: isClone ? payload.graphic.settings.val : null, items: [] } }),
-    this.direction = new EditControlModel({ label: 'Direction', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Direction, val: isClone ? payload.direction.settings.val : 1, min: 1, max: 360 } }),
-    this.scale = new EditControlModel({ label: 'Scale', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Scale, val: isClone ? payload.scale.settings.val : 1, min: 1, max: 5 } })
+    this.initModels(payload);
+    this.initControls();
+  }
 
+  initModels(payload) {
+    let isClone = typeof payload.radius === "object";
+    this.radius = new EditControlModel({ label: 'Radius', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Radius, val: isClone ? payload.radius.settings.val : payload.radius || 10, min: 1, max: 1000 } });
+    this.thickness = new EditControlModel({ label: 'Thickness', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Thickness, val: isClone ? payload.thickness.settings.val : 4, min: 1, max: 150 } });
+    this.divisions = new EditControlModel({ label: 'Divisions', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Divisions, val: isClone ? payload.divisions.settings.val : 0, min: 0, max: 180 } });
+    this.spacing = new EditControlModel({ label: 'Spacing', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Spacing, val: isClone ? payload.spacing.settings.val : 10, min: 1, max: 359 } });
+    this.rotation = new EditControlModel({ label: 'Rotation', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Rotation, val: isClone ? payload.rotation.settings.val : 1, min: 1, max: 360 } });
+    this.graphic = new EditControlModel({ label: 'Graphic', type: EditControlModel.Type.List, settings: { reticleProp: ReticleModel.SettingType.Graphic, val: isClone ? payload.graphic.settings.val : null, items: [] } });
+    this.direction = new EditControlModel({ label: 'Direction', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Direction, val: isClone ? payload.direction.settings.val : 1, min: 1, max: 360 } });
+    this.scale = new EditControlModel({ label: 'Scale', type: EditControlModel.Type.Range, settings: { reticleProp: ReticleModel.SettingType.Scale, val: isClone ? payload.scale.settings.val : 1, min: 1, max: 5 } });
+  }
+
+  initControls() {
     this.controlInFocus = this.radius;
-    this.controls = [
-      this.radius,
-      this.thickness,
-      this.divisions,
-      this.spacing,
-      this.rotation,
-      this.graphic,
-      this.direction,
-      this.scale
-    ];
-  }  
+    this.controls = [this.radius, this.thickness, this.divisions, this.spacing, this.rotation, this.graphic, this.direction, this.scale];
+    this.controls.forEach((control) => { observe(control.settings, (change) => this.onSettingsChange(control, change)); });
+  }
+
+  @action.bound onSettingsChange(control, change) {
+    this.controlInFocus = control;
+  }
 }
