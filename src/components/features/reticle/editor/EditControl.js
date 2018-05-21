1fd54660624492ad21aa61e6c2b88ca32958927d
Derek Knox <derek-knox@users.noreply.github.com>
2018-05-21 15:16:36 -0700

    updated controlInFocus update via manual click (where slider interaction changes also automatically update the controlInFocus from the previous commit)

--- src/components/features/reticle/editor/EditControl.js
+++ src/components/features/reticle/editor/EditControl.js
@@ -10,17 +10,17 @@ import EditControlWidget from './EditControlWidget';
 @keydown('up', 'right', 'down', 'left', 'shift+up', 'shift+right', 'shift+down', 'shift+left')
 export default class EditControl extends Component {
 
-    @action.bound onMouseEnterControl(e, payload) {
+    @action.bound onMouseDownControl(e, payload) {
         this.props.stores.reticlesStore.reticleInFocus.controlInFocus = payload;
     }
 
     render() {
 
-        const item = this.props.item;
+        const item = this.props.item;    
 
         return (
             <div className={'reticle-editor-control-row ' + (this.props.controlInFocus.id === item.id ? 'is-selected' : '')}
-                 onMouseEnter={(e) => this.onMouseEnterControl(e, item)}>
+                 onMouseDown={(e) => this.onMouseDownControl(e, item)}>
                 
                 <div className='reticle-editor-control-label'>
                     {item.label}
