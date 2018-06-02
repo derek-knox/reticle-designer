db1bfb8c05a5017f784e9cc92ab3259cc758e60b
Derek Knox <derek-knox@users.noreply.github.com>
2018-06-02 11:33:36 -0700

    refactored Slider in to SliderWidget

--- src/components/features/reticle/editor/EditControlWidget.js
+++ src/components/features/reticle/editor/EditControlWidget.js
@@ -1,43 +1,17 @@
 import React, { Component } from 'react';
-import { action } from 'mobx';
 import { inject, observer } from 'mobx-react';
-import { keydownScoped } from 'react-keydown';
-
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 import { EditControlModel } from '../../../../state/models/EditControlModel';
 import GraphicWidget from './widgets/GraphicWidget';
+import SliderWidget from "./widgets/SliderWidget";
 
 @inject('stores')
 @observer
 export default class EditControlWidget extends Component {
 
     getControlByType = (payload) => {
-        if (payload.type === EditControlModel.Type.Range)
-            return <Slider value={this.props.stores.reticlesStore.reticleInFocus[payload.settings.reticleProp].settings.val}
-                           min={payload.settings.min}
-                           max={payload.settings.max}
-                           step={payload.settings.step}
-                           onChange={(val) => this.onSliderChange({ val })}
-                           trackStyle={{ backgroundColor: '#99cc33' }}
-                           railStyle={{ backgroundColor: '#333333' }}
-                           handleStyle={{ borderColor: '#99cc33', backgroundColor: '#99cc33' }} />;
-        else if (payload.type === EditControlModel.Type.Grid)
-            return <GraphicWidget />;
-    }
-
-    @keydownScoped('up', 'right') onKeyUpOrRight(e) { this.onSliderChange({ val: this.props.item.settings.val + this.props.item.settings.step }) }
-    @keydownScoped('down', 'left') onKeyDownOrLeft(e) { this.onSliderChange({ val: this.props.item.settings.val - this.props.item.settings.step }) }
-    @keydownScoped('shift+up', 'shift+right') onKeyShiftAndUpOrRight(e) { this.onSliderChange({ val: this.props.item.settings.val + (this.props.item.settings.step * 10) }) }
-    @keydownScoped('shift+down', 'shift+left') onKeyShiftAndDownOrLeft(e) { this.onSliderChange({ val: this.props.item.settings.val - (this.props.item.settings.step * 10) }) }
-
-    @action.bound onSelectChange(payload) {
-        this.props.stores.reticlesStore.reticleInFocus.updateSettingsValue({ val: payload.target.value, reticleProp: this.props.item.settings.reticleProp });
-    }
-
-    @action.bound onSliderChange(payload) {
-        this.props.stores.reticlesStore.reticleInFocus.updateSettingsValue({ val: payload.val, reticleProp: this.props.item.settings.reticleProp });
+        if (payload.type === EditControlModel.Type.Range) { return <SliderWidget item={payload} {...this.props} />; }
+        else if (payload.type === EditControlModel.Type.Grid) { return <GraphicWidget item={payload} {...this.props} />; }
     }
 
     render() {
--- src/components/features/reticle/editor/widgets/SliderWidget.js
+++ src/components/features/reticle/editor/widgets/SliderWidget.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
+import { action } from "mobx";
 import {inject, observer} from 'mobx-react';
+
+import { keydownScoped } from "react-keydown";
+
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 @inject('stores')
 @observer
 export default class SliderWidget extends Component {
 
+    @keydownScoped('up', 'right') onKeyUpOrRight(e) {
+        this.onSliderChange({ val: this.props.item.settings.val + this.props.item.settings.step });
+    }
+    @keydownScoped('down', 'left') onKeyDownOrLeft(e) {
+        this.onSliderChange({ val: this.props.item.settings.val - this.props.item.settings.step });
+    }
+    @keydownScoped('shift+up', 'shift+right') onKeyShiftAndUpOrRight(e) {
+        this.onSliderChange({ val: this.props.item.settings.val + (this.props.item.settings.step * 10) });
+    }
+    @keydownScoped('shift+down', 'shift+left') onKeyShiftAndDownOrLeft(e) {
+        this.onSliderChange({ val: this.props.item.settings.val - (this.props.item.settings.step * 10) });
+    }
+
+    @action.bound onSliderChange(payload) {
+        this.props.stores.reticlesStore.reticleInFocus.updateSettingsValue({ val: payload.val, reticleProp: this.props.item.settings.reticleProp });
+    }
+
     render() {
-        return <div>TODO put slider in here</div>;
+
+        const item = this.props.item;
+
+        return <Slider value={this.props.stores.reticlesStore.reticleInFocus[item.settings.reticleProp].settings.val}
+                       min={item.settings.min}
+                       max={item.settings.max}
+                       step={item.settings.step}
+                       onChange={(val) => this.onSliderChange({ val })}
+                       trackStyle={{ backgroundColor: '#99cc33' }}
+                       railStyle={{ backgroundColor: '#333333' }}
+                       handleStyle={{ borderColor: '#99cc33', backgroundColor: '#99cc33' }} />;
     }
 
 }
