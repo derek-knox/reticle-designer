36473a1fe1d5d4ae2a6ab0dbb5d9ca88c9496f4f
Derek Knox <derek-knox@users.noreply.github.com>
2018-06-03 13:11:31 -0700

    finished WidgetHelper Graphic display and interaction

--- src/components/features/reticle/editor/widgets/WidgetHelper.js
+++ src/components/features/reticle/editor/widgets/WidgetHelper.js
@@ -11,13 +11,30 @@ import Graphic from "../../Graphic";
 @observer
 export default class WidgetHelper extends Component {
 
+    initialGraphicId = null;
+
+    componentDidMount() {
+        this.initialGraphicId = this.props.reticleInFocus.graphic.settings.val;
+    }
+    
     componentDidUpdate() {
         if(this.props.controlInFocus.type !== EditControlModel.Type.Grid && this.props.stores.editReticleStore.isGridControlOpen) {
             this.props.stores.editReticleStore.isGridControlOpen = false;
         }
     }
-
+    
     @action.bound onClickClose(e) {
+        this.props.reticleInFocus.graphic.settings.val = this.initialGraphicId;
+        this.props.stores.editReticleStore.isGridControlOpen = false;
+    }
+
+    @action.bound onMouseOverGraphic(e, payload) {
+        this.props.reticleInFocus.graphic.settings.val = payload;
+    }
+
+    @action.bound onClickGraphic(e, payload) {
+        this.props.reticleInFocus.graphic.settings.val = payload;
+        this.initialGraphicId = payload;
         this.props.stores.editReticleStore.isGridControlOpen = false;
     }
 
@@ -37,18 +54,34 @@ export default class WidgetHelper extends Component {
                                 onClick={this.onClickClose}>x</Button>
                     </div>
                     <div className="widget-helper-body">
-                        <div className="widget-helper-grid">
+                        <div className={"widget-helper-grid " + (!this.props.stores.editReticleStore.isGridControlOpen ? 'hidden' : ' ')}>
+
+                            <div key={'gfx-0'}
+                                 className={"widget-helper-grid-item " +
+                                     (this.initialGraphicId === null ? 'is-initially-selected ' : ' ') +
+                                     (this.props.reticleInFocus.graphic.settings.val === null ? 'is-selected' : '')
+                                 }
+                                 onMouseOver={(e) => this.onMouseOverGraphic(e, null)}
+                                 onClick={(e) => this.onClickGraphic(e, null)}>
+                            None</div>
+
                             {this.props.stores.editReticleStore.graphics.map((item) => {
                                     return (
-                                        <div key={item.id} className="widget-helper-grid-item">
+                                        <div key={item.id}
+                                             className={"widget-helper-grid-item " + 
+                                                        (this.initialGraphicId === item.id ? 'is-initially-selected ' : ' ') +
+                                                        (this.props.reticleInFocus.graphic.settings.val === item.id ? 'is-selected' : '')
+                                                       }
+                                             onMouseOver={(e) => this.onMouseOverGraphic(e, item.id)}
+                                             onClick={(e) => this.onClickGraphic(e, item.id)}>
                                             <svg className="widget-helper-graphic" width="100%" height="100%">
                                                 <Graphic key={item.id}
-                                                    gfxId={item.id}
-                                                    radius={0}
-                                                    center={{ x: 0, y: 0 }}
-                                                    angle={0}
-                                                    direction={0}
-                                                    scale={1} />
+                                                         gfxId={item.id}
+                                                         radius={0}
+                                                         center={{ x: 0, y: 0 }}
+                                                         angle={0}
+                                                         direction={0}
+                                                         scale={1} />
                                             </svg>
                                         </div>
                                     );
