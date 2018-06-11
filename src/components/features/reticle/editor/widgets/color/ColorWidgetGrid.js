72260c20f1dfa8cfff63caa82244c927ca92df66
Derek Knox <derek-knox@users.noreply.github.com>
2018-06-11 16:49:56 -0700

    updated ColorWidgetGrid w/placeholder ColorWheel instances

--- src/components/features/reticle/editor/widgets/color/ColorWidgetGrid.js
+++ src/components/features/reticle/editor/widgets/color/ColorWidgetGrid.js
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
 
-import ColorPalette from "./ColorPalette";
+import ColorWheel from "./ColorWheel";
 
 @inject('stores')
 @observer
 
 export default class ColorWidgetGrid extends Component {
 
+    onMouseOverPalette(e, payload) {
+        console.log('over', payload);
+    }
+    
+    onClickPalette(e, payload) {
+        this.props.stores.colorStore.updateColorPaletteInFocus({ id: payload });
+    }
+
     render() {
 
         return (
-            <div className="widget-helper-grid widget-helper-grid-color">
-                ...
+            <div className="widget-helper-grid">
+                {this.props.stores.colorStore.items.map((item) => {
+                        return (
+                            <div key={item.id}
+                                 className="widget-helper-grid-item"
+                                 onMouseOver={(e) => this.onMouseOverPalette(e, item.id)}
+                                 onClick={(e) => this.onClickPalette(e, item.id)}>
+                                    <ColorWheel palette={item} />
+                            </div>
+                        );
+                    })
+                }
             </div>
         );
     }
--- src/state/stores/ColorStore.js
+++ src/state/stores/ColorStore.js
@@ -10,14 +10,14 @@ export class ColorStore {
     constructor(stores) {
         this.stores = stores;
 
-        this.add({ colors: ['#F8B195', '#F67280', '#C06C84', '#6C5B7B', '#355C7D'] });
-        this.add({ colors: ['#A8A7A7', '#CC527A', '#E8175D', '#474747', '#363636'] });
-        this.add({ colors: ['#99B898', '#FECEAB', '#FF847C', '#E84A5F', '#2A363B'] });
-        this.add({ colors: ['#A8E6CE', '#DCEDC2', '#FFD3B5', '#FFAAA6', '#FF8C94'] });
-        this.add({ colors: ['#A7226E', '#EC2049', '#F26B38', '#F7DB4F', '#2F9599'] });
-        this.add({ colors: ['#E1F5C4', '#EDE574', '#F9D423', '#FC913A', '#FF4E50'] });
-        this.add({ colors: ['#E5FCC2', '#9DE0AD', '#45ADA8', '#547980', '#594F4F'] });
-        this.add({ colors: ['#FE4365', '#FC9D9A', '#F9CDAD', '#C8C8A9', '#83AF9B'] });
+        this.add({ id: 1, colors: ['#F8B195', '#F67280', '#C06C84', '#6C5B7B', '#355C7D'] });
+        this.add({ id: 2, colors: ['#A8A7A7', '#CC527A', '#E8175D', '#474747', '#363636'] });
+        this.add({ id: 3, colors: ['#99B898', '#FECEAB', '#FF847C', '#E84A5F', '#2A363B'] });
+        this.add({ id: 4, colors: ['#A8E6CE', '#DCEDC2', '#FFD3B5', '#FFAAA6', '#FF8C94'] });
+        this.add({ id: 5, colors: ['#A7226E', '#EC2049', '#F26B38', '#F7DB4F', '#2F9599'] });
+        this.add({ id: 6, colors: ['#E1F5C4', '#EDE574', '#F9D423', '#FC913A', '#FF4E50'] });
+        this.add({ id: 7, colors: ['#E5FCC2', '#9DE0AD', '#45ADA8', '#547980', '#594F4F'] });
+        this.add({ id: 8, colors: ['#FE4365', '#FC9D9A', '#F9CDAD', '#C8C8A9', '#83AF9B'] });
 
         this.colorPaletteInFocus = this.items[0];
     }
