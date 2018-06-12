cc97553154731b60bce1cdab5beaaf97317d290a
Derek Knox <derek-knox@users.noreply.github.com>
2018-06-11 17:48:42 -0700

    updated ColorWidgetGrid to properly update on hover and revert if not explicitly selected

--- src/components/features/reticle/editor/widgets/color/ColorWidgetGrid.js
+++ src/components/features/reticle/editor/widgets/color/ColorWidgetGrid.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
+import { action, observe, observable } from "mobx";
 import {inject, observer} from 'mobx-react';
+
+import classnames from "classnames";
 
 import ColorWheel from "./ColorWheel";
 
@@ -7,13 +10,44 @@ import ColorWheel from "./ColorWheel";
 @observer
 
 export default class ColorWidgetGrid extends Component {
+    
+    @observable initialPaletteId = this.props.stores.colorStore.colorPaletteInFocus.id;
+    unobserve = null;
+
+    constructor(props) {
+        super(props);
+        this.unobserve = observe(this.props.stores.reticlesStore, 'reticleInFocus', this.onChangeReticleInFocus);
+    }
+
+    componentDidUpdate() {
+        if (this.props.isReset) {
+            this.reset();
+        }
+    }
+
+    componentWillUnmount() {
+        this.unobserve();
+    }
+
+    reset() {
+        this.props.stores.colorStore.updateColorPaletteInFocus({ id: this.initialPaletteId });
+    }
 
     onMouseOverPalette(e, payload) {
-        console.log('over', payload);
+        // Prevent quick hover after selection causing graphic to update
+        if (this.props.stores.editReticleStore.isGridControlOpen) {
+            this.props.stores.colorStore.updateColorPaletteInFocus({ id: payload });
+        }
     }
     
     onClickPalette(e, payload) {
         this.props.stores.colorStore.updateColorPaletteInFocus({ id: payload });
+        this.initialPaletteId = payload;
+        this.props.stores.editReticleStore.isGridControlOpen = false;
+    }
+
+    @action.bound onChangeReticleInFocus(payload) {
+        this.reset();
     }
 
     render() {
@@ -23,7 +57,9 @@ export default class ColorWidgetGrid extends Component {
                 {this.props.stores.colorStore.items.map((item) => {
                         return (
                             <div key={item.id}
-                                 className="widget-helper-grid-item"
+                                 className={classnames("widget-helper-grid-item ",
+                                    { 'is-initially-selected': this.initialPaletteId === item.id },
+                                    { 'is-selected': true })}
                                  onMouseOver={(e) => this.onMouseOverPalette(e, item.id)}
                                  onClick={(e) => this.onClickPalette(e, item.id)}>
                                     <ColorWheel palette={item} />
