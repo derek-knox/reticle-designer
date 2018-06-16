19faa3a2613b33c66b5a3883df6bb4f061f5d1b2
Derek Knox <derek-knox@users.noreply.github.com>
2018-06-16 09:30:08 -0700

    updated swatches to be round, refactored ColorSwatch to be presentational and updated ColorPalette to manage resetting swatch isSelected and isInitiallySelected values as props for ColorSwatch

--- src/components/features/reticle/editor/widgets/color/ColorPalette.js
+++ src/components/features/reticle/editor/widgets/color/ColorPalette.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {action, observable} from 'mobx';
 import {inject, observer} from 'mobx-react';
 
 import ColorSwatch from './ColorSwatch';
@@ -7,13 +8,45 @@ import ColorSwatch from './ColorSwatch';
 @observer
 export default class ColorPalette extends Component {
 
+    @observable initialSwatchIndex = this.props.stores.reticlesStore.reticleInFocus.color.settings.val;
+
+    @action updateColorForReticle(payload) {
+        this.props.stores.reticlesStore.reticleInFocus.updateSettingsValue({ val: payload, reticleProp: 'color' });
+    }
+
+    @action.bound onMouseEnter() {
+        this.initialSwatchIndex = this.props.stores.reticlesStore.reticleInFocus.color.settings.val;
+    }
+    
+    @action.bound onMouseLeave() {
+        this.updateColorForReticle(this.initialSwatchIndex);
+    }
+
+    @action.bound onMouseOverSwatch(payload){
+        this.updateColorForReticle(payload);
+    }
+    
+    @action.bound onClickSwatch(payload){
+        this.updateColorForReticle(payload);
+        this.initialSwatchIndex = payload;
+    }
+
     render() {
 
+        const colorIndexInFocus = this.props.stores.reticlesStore.reticleInFocus.color.settings.val;
+
         return (
-            <div className='widget-helper-grid-color-row'>
+            <div className='widget-helper-grid-color-row'
+                 onMouseEnter={this.onMouseEnter}
+                 onMouseLeave={this.onMouseLeave}>
                 {this.props.palette.colors.map((color, idx) => {
-                    return <ColorSwatch key={idx} colorIndex={idx} />;
-                })}
+                    return <ColorSwatch key={idx}
+                                        color={color}
+                                        isSelected={idx === colorIndexInFocus}
+                                        isInitiallySelected={idx === this.initialSwatchIndex}
+                                        onComponentClick={() => this.onClickSwatch(idx)}
+                                        onComponentMouseOver={() => this.onMouseOverSwatch(idx)} />;
+                }, this)}
             </div>
         );
     }
--- src/components/features/reticle/editor/widgets/color/ColorSwatch.js
+++ src/components/features/reticle/editor/widgets/color/ColorSwatch.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {action} from 'mobx';
 import {inject, observer} from 'mobx-react';
 
 import classnames from 'classnames';
@@ -8,22 +7,14 @@ import classnames from 'classnames';
 @observer
 export default class ColorSwatch extends Component {
 
-    @action.bound onClickSwatch() {
-        this.props.stores.reticlesStore.reticleInFocus.updateSettingsValue({ val: this.props.colorIndex, reticleProp: 'color' });
-    }
-
     render() {
-
-        const color = this.props.stores.colorStore.getColor(this.props.colorIndex);
-
-        console.log('TODO, likely plan out initiColorIndex in palette component and pass in? or something similar')
-
         return (
             <div className={classnames("color-item",
-                    { 'is-initially-selected': this.props.stores.reticlesStore.reticleInFocus.color.settings.val === this.props.colorIndex },
-                    { 'is-selected': this.props.stores.reticlesStore.reticleInFocus.color.settings.val === this.props.colorIndex })}
-                 onClick={this.onClickSwatch}>
-                <div className='color-swatch' style={{ backgroundColor: color }}></div>
+                    { 'is-initially-selected': this.props.isInitiallySelected },
+                    { 'is-selected': this.props.isSelected })}
+                 onClick={this.props.onComponentClick}
+                 onMouseOver={this.props.onComponentMouseOver}>
+                <div className='color-swatch' style={{ backgroundColor: this.props.color }}></div>
             </div>
         );
     }
