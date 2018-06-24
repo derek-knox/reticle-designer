368b3b586a752c1b783955f2da86cf1e1fce26f1
Derek Knox <derek-knox@users.noreply.github.com>
2018-06-24 15:29:43 -0700

    added draggable layers

--- src/components/features/reticle/editor/LayersContainer.js
+++ src/components/features/reticle/editor/LayersContainer.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { action } from 'mobx';
 import {inject, observer} from 'mobx-react';
 
+import { SortableContainer, SortableElement, SortableHandle, arrayMove } from 'react-sortable-hoc';
+
 import Button from "@material-ui/core/Button";
 
 import LayerListItem from './LayerListItem';
@@ -18,6 +20,10 @@ export default class LayersContainer extends Component {
         this.props.stores.editReticleStore.takeSnapshot();
     }
 
+    @action.bound onSortEnd(payload) {
+        this.props.stores.reticlesStore.items = arrayMove(this.props.stores.reticlesStore.items, payload.oldIndex, payload.newIndex);
+    }
+
     render() {
         return (
             <div className="container reticle-editor-layers-container">
@@ -27,11 +33,15 @@ export default class LayersContainer extends Component {
                         <div className='reticle-editor-heading'>Layers</div>
                         
                         <div className="reticle-editor-layers">
-                            <div className="reticle-editor-layers-items">
-                                {this.props.stores.reticlesStore.items.map(item => {
-                                    return <LayerListItem key={item.id} item={item} />
-                                })}
-                            </div>
+                            
+                            <SortableList items={this.props.stores.reticlesStore.items}
+                                          useDragHandle={true}
+                                          onSortEnd={this.onSortEnd}
+                                          lockAxis='y'
+                                          transitionDuration={220}
+                                          lockToContainerEdges={true}
+                                          helperClass='dragging-layer' />
+
                             <Button className="reticle-editor-layers-button"
                                     onClick={this.onClickClone}>Clone {this.props.stores.reticlesStore.reticleInFocus.label}</Button>
                             <Button className="reticle-editor-layers-button"
@@ -43,5 +53,23 @@ export default class LayersContainer extends Component {
             </div>
         );
     }
-
 }
+
+const DragHandle = SortableHandle(() => <div className='reticle-editor-layer-list-item-draggable'>::</div>);
+
+const SortableItem = SortableElement(({ value }) => (
+    <div className='reticle-editor-layer-list-item-draggable-container'>
+        <DragHandle />
+        <LayerListItem item={value} />
+    </div>
+));
+
+const SortableList = SortableContainer(({ items }) => {
+    return (
+        <div className="reticle-editor-layers-items">
+            {items.map((item, index) => {
+                return <SortableItem key={item.id} index={index} value={item} />;
+            })}
+        </div>
+    );
+});
