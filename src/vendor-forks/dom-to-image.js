2a2c0f3bc05aab5f983335bc7d23e8e62887a446
Derek Knox <derek-knox@users.noreply.github.com>
2018-06-26 17:08:19 -0700

    minor refactoring

--- src/vendor-forks/dom-to-image.js
+++ src/vendor-forks/dom-to-image.js
@@ -192,7 +192,7 @@
         }
 
         function cloneChildren(original, clone, filter) {
-            var children = original.tagName === "use" ? processShadowChild(original) : original.childNodes;
+            var children = original.tagName === "use" ? copyShadowChild(original) : original.childNodes;
 
             if (children.length === 0) return Promise.resolve(clone);
 
@@ -216,9 +216,9 @@
             }
         }
 
-        function processShadowChild(original) {
-            var useTarget = document.getElementById(original.href.baseVal.replace('#', ''));
-            return [useTarget.cloneNode(true)];
+        function copyShadowChild(original) {
+            var child = document.getElementById(original.href.baseVal.replace('#', ''));
+            return [child.cloneNode(true)];
         }
 
         function processClone(original, clone) {
